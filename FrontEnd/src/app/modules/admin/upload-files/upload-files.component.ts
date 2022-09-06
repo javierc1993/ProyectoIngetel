63d4c83361c0940c8c2d8e38555c88d01a02ad82
DiegoV0217 <diegofernandov@unicauca.edu.co>
2022-09-06 05:45:04 -0500

    maquetación modulo carga archivos

--- FrontEnd/src/app/modules/admin/upload-files/upload-files.component.ts
+++ FrontEnd/src/app/modules/admin/upload-files/upload-files.component.ts
@@ -1,10 +1,40 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-upload-files',
     templateUrl: './upload-files.component.html',
     encapsulation: ViewEncapsulation.None,
 })
-export class UploadFilesComponent {
-    constructor() {}
+export class UploadFilesComponent implements OnInit {
+    contactForm!: FormGroup;
+
+    constructor(private readonly fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.contactForm = this.initForm();
+        // this.onPathValue();
+        // this.onSetValue();
+    }
+
+    onPathValue(): void {
+        this.contactForm.patchValue({ name: 'Bezael' });
+    }
+
+    onSetValue(): void {
+        // this.contactForm.setValue({ comment: 'Hola mundo' });
+    }
+
+    onSubmit(): void {
+        console.log('Form ->', this.contactForm.value);
+    }
+
+    initForm(): FormGroup {
+        return this.fb.group({
+            name: ['', [Validators.required, Validators.minLength(3)]],
+            checkAdult: ['', [Validators.required]],
+            department: [''],
+            comment: ['', [Validators.required]],
+        });
+    }
 }
--- FrontEnd/src/app/modules/admin/upload-files/upload-files.module.ts
+++ FrontEnd/src/app/modules/admin/upload-files/upload-files.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { UploadFilesComponent } from 'app/modules/admin/upload-files/upload-files.component';
-import { FuseCardModule } from '@fuse/components/card';
 import { uploadFilesRoutes } from './upload-files.routing';
+import { SharedModule } from 'app/shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [UploadFilesComponent],
-    imports: [RouterModule.forChild(uploadFilesRoutes), FuseCardModule],
+    imports: [
+        RouterModule.forChild(uploadFilesRoutes),
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        MatRadioModule,
+        MatSelectModule,
+        SharedModule,
+    ],
 })
 export class UploadFilesModule {}
