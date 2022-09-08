b782096ff6e037c4c38cf38bfc6767a3b98f2c8c
DiegoV0217 <diegofernandov@unicauca.edu.co>
2022-09-07 21:00:04 -0500

    update service upload file whit GLOBAL

--- FrontEnd/src/app/modules/admin/upload-files/uploadFile.services.ts
+++ FrontEnd/src/app/modules/admin/upload-files/uploadFile.services.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-
+import { variablesGlobales } from 'GLOBAL';
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
-export class uploadFileService
-{
-
+export class uploadFileService {
     /**
      * Constructor
      */
-    constructor(private _httpClient: HttpClient)
-    {
-    }
+    constructor(private _httpClient: HttpClient) {}
 
     // -----------------------------------------------------------------------------------------------------
     // @ Public methods
@@ -22,10 +18,11 @@ export class uploadFileService
     /**
      * send file to backend
      */
-   
 
-    sendFilePost(body:FormData): Observable<any>{
-        return this._httpClient.post('http://127.0.0.1:3000/api/v1/production/upload', body)
+    sendFilePost(body: FormData): Observable<any> {
+        return this._httpClient.post(
+            variablesGlobales.urlBackend + '/production/upload',
+            body
+        );
     }
-
 }
