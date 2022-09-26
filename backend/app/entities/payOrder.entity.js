89c07ed127af770daaf3257389364df1e2af8ed6
ruben-gutierrez <rubengutierrez@unicauca.edu.co>
2022-09-26 10:36:24 -0500

    correct entity

--- backend/app/entities/payOrder.entity.js
+++ backend/app/entities/payOrder.entity.js
@@ -2,28 +2,29 @@
 
 class PayOrderEntity {
   constructor (data) {
-    reference = data['PO'] || null;
-    scenery = data['Escenario '] || null;
-    value = data[' VALOR PO '] || null;
-    band = data['Banda'] || null;
-    Sites = {
+    this.proyect = data['Proyecto'] || null;
+    this.reference = data['PO'] || null;
+    this.scenery = data['Escenario '] || null;
+    this.value = data[' VALOR PO '] || null;
+    this.band = data['Banda'] || null;
+    this.Sites = {
       name: po['SITE NAME'] || null,
       smp: po['SMP'] || null,
       region: po['Regional'] || null,
     };
-    Instalations = {
+    this.Instalations = {
       date: po['instalacion'] ? new Date(po['instalacion'] + ' 00:00:00') : null
     };
-    Integrations = {
+    this.Integrations = {
       date: po['Fecha de Integracion'] ? new Date(po['Fecha de Integracion'] + ' 00:00:00') : null
     };
-    MosHws = {
+    this.MosHws = {
       date: po['mos_HW'] ? new Date(po['mos_HW'] + ' 00:00:00') : null
     };
-    OnAirs = {
+    this.OnAirs = {
       date: po['ON AIR'] || null
     };
-    Leaders = {
+    this.Leaders = {
       name: po['Lider '] || null
     };
   }
