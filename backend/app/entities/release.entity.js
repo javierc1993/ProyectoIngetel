e7c07e46fdd687af90b3347d88dad920e266794d
ruben-gutierrez <rubengutierrez@unicauca.edu.co>
2022-11-03 21:32:13 -0500

    new field percent

--- backend/app/entities/release.entity.js
+++ backend/app/entities/release.entity.js
@@ -5,14 +5,15 @@ class ReleaseEntity {
     this.proyect = data.proyect || null;
     this.woName = data.woName || null;
     this.vendorSapName = data.vendorSapName || null;
-    this.iaDate = data.iaDate ? excelDateToJSDate(data.iaDate ) : null;
-    this.grDate = data.grDate ? excelDateToJSDate(data.grDate ) : null;
+    this.iaDate = data.iaDate ? excelDateToJSDate(data.iaDate) : null;
+    this.grDate = data.grDate ? excelDateToJSDate(data.grDate) : null;
     this.sgrNumber = data.sgrNumber || null;
     this.percent = data.percent || null;
     this.date = data.date ? excelDateToJSDate(data.date) : null;
     this.ppa = data.ppa ? excelDateToJSDate(data.ppa) : null;
     this.payOrderId = data.payOrder || null;
     this.releaseTypeId = data.releaseType || null;
+    this.totalPercent = data.totalPercent;
   }
 }
 
