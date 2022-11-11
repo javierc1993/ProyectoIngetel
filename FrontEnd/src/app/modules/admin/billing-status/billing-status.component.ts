9195f5d0dc7f2f1048868e8fe11f181b4dea980b
DiegoV0217 <diegofernandov@unicauca.edu.co>
2022-11-11 08:27:11 -0500

    already billing-status

--- FrontEnd/src/app/modules/admin/billing-status/billing-status.component.ts
+++ FrontEnd/src/app/modules/admin/billing-status/billing-status.component.ts
@@ -10,7 +10,7 @@ import {FormGroup, FormControl,ReactiveFormsModule} from '@angular/forms';
 import { ExporterService } from 'services/exporter.service';
 
 export interface transaction {
-    fechaFactura: string;
+    fechaFactura: Date;
     numeroFactura:string;
     subtotal: string;
     totalFactura: string;
@@ -22,7 +22,7 @@ export interface transaction {
     sitio: string;
     proyecto: string;
     porcentajeFactura: string;
-    fechaPago: string;
+    fechaPago: Date;
     estado: string;
 }
 
@@ -51,16 +51,21 @@ export class BillingStatusComponent implements OnInit {
     fechaDesdeInstalacion:new FormControl<Date | null>(null),
   });
   constructor(private _httpClient: HttpClient,private _formBuilder: UntypedFormBuilder, private excelService:ExporterService) { 
-    this.recentTransactionsTableColumns=['Fecha factura','Numero factura', 'Subtotal', 'Total factura', 'RTF', 'RTIVA', 'PO', 'SMP', 'Sitio', 'Proyecto', 'Porcentaje factura', 'Fecha pago', '# Documento', 'Valor pagado', 'Estado'];
+    this.recentTransactionsTableColumns=['Fecha factura','Numero factura', 'Subtotal', 'Total factura', 'RTF', 'RTIVA', 'PO', 'SMP', 'Sitio', 'Proyecto', 'Porcentaje factura', 'Fecha pago', '# Documento', 'Valor pagado', 'Estado'];     
+    const initDateBilling = this.getFilterLastYear();
+    //console.log(JSON.stringify(initDateBilling))
+    this.getData(initDateBilling);
+  }
+  
+  getFilterLastYear(){
     var thisDate = new Date();
     var thisDateFormat = this.formatoFecha(thisDate);
     var dateLastYear = new Date();
     dateLastYear.setMonth(dateLastYear.getMonth()-12);    
     var thisDateLastYearFormat = this.formatoFecha(dateLastYear); 
-    const initDateBilling = {'fechaDesdeFactura':thisDateLastYearFormat,'fechaHastaFactura':thisDateFormat};
-    //console.log(JSON.stringify(initDateBilling))
-    this.cargueCompleto(initDateBilling);
+    return {'fechaDesdeFactura':thisDateLastYearFormat,'fechaHastaFactura':thisDateFormat};
   }
+
   formatoFecha(fecha) {    
     var thisDate = fecha.getDate()+'/';     
     thisDate += (fecha.getMonth() + 1)+'/';
@@ -68,11 +73,10 @@ export class BillingStatusComponent implements OnInit {
     return thisDate;
   }
 
-
   ngOnInit(): void {
     this.filterForm = this._formBuilder.group({            
-            poID:[''],
-            smpID:[''],
+            PO:[''],
+            SMP:[''],
             fechaDesdeFactura:[''],
             fechaHastaFactura:[''],
             fechaDesdePago:[''],
@@ -85,51 +89,54 @@ export class BillingStatusComponent implements OnInit {
           });
   }
 
-  cargueCompleto(objectToFilter){
+  getData(objectToFilter){
 
    // console.log("cargando el componente billing status: "+JSON.stringify(objectToFilter));
     
     this._httpClient.post(variablesGlobales.urlBackend + '/invoice/',objectToFilter)
       .subscribe((response:any) => { 
-        console.log("response: ") 
-        console.log(response)  
+        //  console.log("response: ") 
+        //  console.log(response)  
         this.listInvoice = response.reduce((acc, el)=>({
           ...acc, 
-          [el.reference]:el,
-        }),{});
-
-        
-        this.datosHoja = response.map(function(thisBill : any){ 
-          var fechaFactura = new Date(thisBill.date);  
-          fechaFactura = new Date (fechaFactura.getTime() + (3600000 * 5) );
-          var fechaPago = thisBill.pay ? new Date (thisBill.pay.createdAt):null;
-          fechaPago = fechaPago ? new Date (fechaPago.getTime() + (3600000 * 5) ):null;
-          //console.log("thisdate: "+thisDate) ;      
-          return {
-            fechaFactura: fechaFactura,
-            numeroFactura: thisBill.invoice,
-            subtotal: thisBill.subTotal,
-            totalFactura: thisBill.total,
-            rtf: thisBill.rtf,
-            rtiva: thisBill.rtIva,
-            //totalPagar:thisBill.value,
-            poID: thisBill.payOrder.reference,
-            smpID: thisBill.payOrder.site.smp,
-            sitio: thisBill.payOrder.site.name,
-            proyecto: thisBill.payOrder.scenery,
-            porcentajeFactura: thisBill.percentInvoice,
-            fechaPago: fechaPago,
-            documentNumber: thisBill.pay ? thisBill.pay.documentNumber:null,
-            valorPagado: thisBill.pay ? thisBill.pay.amountUtilized:null,
-            estado: thisBill.pay ? 'pagado':'pendiente'
-          }
-        });         
-        this.recentTransactionsDataSource = new MatTableDataSource(this.datosHoja);
-        this.recentTransactionsDataSource.paginator = this.paginator;
-              //this.updateGrafica1();
+          [el.invoice]:el,
+        }),{});        
+        this.loadDataTable();
       },
       (error) => {console.log(error);}                
     );
+  }
+
+  loadDataTable(): void {
+    //console.log(this.listInvoice);
+    this.datosHoja = Object.values(this.listInvoice).map(function(thisBill : any){ 
+    var fechaFactura = new Date(thisBill.date);  
+    fechaFactura = new Date (fechaFactura.getTime() + (3600000 * 5) );
+    var fechaPago = thisBill.pay ? new Date (thisBill.pay.datePay):null;
+    fechaPago = fechaPago ? new Date (fechaPago.getTime() + (3600000 * 5) ):null;
+    //console.log("thisdate: "+thisDate) ;      
+    return {
+      fechaFactura: fechaFactura,
+      numeroFactura: thisBill.invoice,
+      subtotal: thisBill.subTotal,
+      totalFactura: thisBill.total,
+      rtf: thisBill.rtf,
+      rtiva: thisBill.rtIva,
+      //totalPagar:thisBill.value,
+      poID: thisBill.payOrder.reference,
+      smpID: thisBill.payOrder.site.smp,
+      sitio: thisBill.payOrder.site.name,
+      proyecto: thisBill.payOrder.scenery,
+      porcentajeFactura: thisBill.percentInvoice,
+      fechaPago: fechaPago,
+      documentNumber: thisBill.pay ? thisBill.pay.documentNumber:null,
+      valorPagado: thisBill.pay ? thisBill.pay.amountUtilized:null,
+      estado: thisBill.pay ? 'pagado':'pendiente'
+    }
+    }); 
+    //console.log(this.datosHoja);        
+    this.recentTransactionsDataSource = new MatTableDataSource(this.datosHoja);
+    this.recentTransactionsDataSource.paginator = this.paginator;
   }
   toggleDrawerOpen(): void {this.drawerOpened = !this.drawerOpened;}
   drawerOpenedChanged(opened: boolean): void{this.drawerOpened = opened;}
@@ -146,46 +153,23 @@ export class BillingStatusComponent implements OnInit {
     console.log("descargando")
   }
   getDataFilter(){
-    var formFiltros = new Object();
     
-    if(this.filterForm.value.poID){
-      Object.defineProperty(formFiltros, 'PO', {
-        value:this.filterForm.value.poID,
-        writable: false
-      });
-    };
-    if(this.filterForm.value.smpID){
-      Object.defineProperty(formFiltros, 'SMP', {
-        value:this.filterForm.value.smpID,
-        writable: false
-      });
-    };
     if(this.filterForm.value.fechaDesdeFactura){
-      Object.defineProperty(formFiltros, 'fechaDesdeFactura', {
-        value:this.filterForm.value.fechaDesdeFactura.format('DD/MM/YYYY'),
-        writable: false
-      });
+      this.filterForm.value.fechaDesdeFactura = this.filterForm.value.fechaDesdeFactura.format('DD/MM/YYYY');
     };
     if(this.filterForm.value.fechaHastaFactura){
-      Object.defineProperty(formFiltros, 'fechaHastaFactura', {
-        value:this.filterForm.value.fechaHastaFactura.format('DD/MM/YYYY'),
-        writable: false
-      });
+      this.filterForm.value.fechaHastaFactura = this.filterForm.value.fechaHastaFactura.format('DD/MM/YYYY');      
     };
-    // if(this.filterForm.value.fechaDesdePago){      
-    //   Object.defineProperty(formFiltros, 'fechaDesdePago', {
-    //     value:this.filterForm.value.fechaDesdePago.format('DD/MM/YYYY'),
-    //     writable: false
-    //   });
+    // if(this.filterForm.value.fechaDesdePago){ 
+    //  this.filterForm.value.fechaDesdePago = this.filterForm.value.fechaDesdePago.format('DD/MM/YYYY');
     // };
-    // if(this.filterForm.value.fechaHastaPago){      
-    //   Object.defineProperty(formFiltros, 'fechaHastaPago', {
-    //     value:this.filterForm.value.fechaHastaPago.format('DD/MM/YYYY'),
-    //     writable: false
-    //   });
+    // if(this.filterForm.value.fechaHastaPago){   
+    //  this.filterForm.value.fechaHastaPago = this.filterForm.value.fechaHastaPago.format('DD/MM/YYYY');   
     // };
+    console.log(this.filterForm.value);
 
-    console.log(formFiltros);
+    //this.getData(this.filterForm.value);
+    
 
   }
 }
